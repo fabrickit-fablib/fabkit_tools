ece7d5937d4940080f585055fb31647821e602de
Shunya Kitada <syun.kitada@gmail.com>
2016-01-04 23:07:48 +0900

    update

--- user_fabkit.py
+++ user_fabkit.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
-from fabkit import env, filer, run
+import re
+from fabkit import env, filer, run, sudo
 from fablib.base import SimpleBase
 from fablib import git
 from fablib.python import Python
@@ -10,19 +11,30 @@ class UserFabkit(SimpleBase):
     def __init__(self):
         self.packages = {
             'CentOS Linux 7.*': [
+                'epel-release',
                 'httpd',
+                'mod_wsgi',
+                'redis',
+                'nodejs',
+                'npm',
             ],
             'Ubuntu 14.*': [
-                'apache2'
+                'apache2',
+                'libapache2-mod-wsgi',
+                'redis-server',
+                'nodejs',
+                'npm',
             ]
         }
 
         self.services = {
             'CentOS Linux 7.*': [
                 'httpd',
+                'redis',
             ],
             'Ubuntu 14.*': [
-                'apache2'
+                'apache2',
+                'redis-server',
             ]
         }
 
@@ -41,10 +53,11 @@ class UserFabkit(SimpleBase):
         python.install(
             requirements='{0}/fabfile/requirements.txt'.format(repo))
 
-        run('cd {0} && /opt/fabkit/bin/fab -l'.format(repo))
+        run('cd {0} && /opt/fabkit/bin/fab genconfig:fabfile.ini &&'
+            ' sed -i "/^\[web\]/,/^\[/s/#hostname =.*/hostname = */g" fabfile.ini'.format(repo))
 
         data = {
-            'port': 3306,
+            'port': 8080,
             'repo': repo,
             'user': env.user,
             'group': env.user,
@@ -53,9 +66,41 @@ class UserFabkit(SimpleBase):
             'threads': 1,
         }
 
-        if filer.template(src='httpd.conf',
-                          dest='/etc/httpd/conf.d/{0}_httpd.conf'.format(env.user),
-                          data=data):
-            self.handlers['restart_httpd'] = True
+        run('cd {0}/fabfile/core/webapp/ &&'
+            ' /opt/fabkit/bin/python manage.py migrate &&'
+            ' echo "from django.contrib.auth.models import User;'
+            '       User.objects.create_superuser(\'admin\', \'admin@localhost\', \'admin\')"'
+            '       | /opt/fabkit/bin/python manage.py shell &&'
+            ' /opt/fabkit/bin/python manage.py collectstatic --noinput'.format(repo))
+
+        sudo('chmod 755 /home/{0}'.format(env.user))
+
+        if re.match('CentOS .*', env.node['os']):
+            log_prefix = '/var/log/httpd/{0}'.format(env.user)
+            data['error_log'] = '{0}-error.log'.format(log_prefix)
+            data['custom_log'] = '{0}-access.log'.format(log_prefix)
+
+            if filer.template(src='httpd.conf',
+                              dest='/etc/httpd/conf.d/{0}_httpd.conf'.format(env.user),
+                              data=data):
+                self.handlers['restart_httpd'] = True
+
+        elif re.match('Ubuntu .*', env.node['os']):
+            log_prefix = '/var/log/apache2/{0}'.format(env.user)
+            data['error_log'] = '{0}-error.log'.format(log_prefix)
+            data['custom_log'] = '{0}-access.log'.format(log_prefix)
+
+            if filer.template(src='httpd.conf',
+                              dest='/etc/apache2/sites-enabled/{0}_httpd.conf'.format(env.user),
+                              data=data):
+                self.handlers['restart_apache2'] = True
 
         self.exec_handlers()
+
+        # install
+        sudo('npm install -g coffee-script'.format(repo))
+        run('cd {0}/fabfile/core/webapp && npm install'.format(repo))
+
+        # install node packages for develop
+        sudo('npm install -g grunt-cli'.format(repo))
+        run('cd {0}/fabfile/core/webapp/node_chat && npm install'.format(repo))
